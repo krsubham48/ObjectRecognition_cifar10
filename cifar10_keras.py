fe5952c96e9997318f46ba2f2aa60148b2838c1b
Kumar Subham <33080005+krsubham48@users.noreply.github.com>
2018-03-18 22:18:28 +0530

    keras implementation

---
+++ cifar10_keras.py
@@ -0,0 +1,81 @@
+import numpy as np
+import os
+import _pickle as cPickle
+from matplotlib import pyplot as plt
+
+class CifarLoader(object):
+    def __init__(self, source_files):
+        self._source = source_files
+        self._i = 0
+        self.images = None
+        self.labels = None
+        
+    def load(self):
+        data = [unpickle(f) for f in self._source]
+        images = np.vstack([d["data"] for d in data])
+        n = len(images)
+        self.images = images.reshape(n, 3, 32, 32).transpose(0, 2, 3, 1).astype(float) / 255
+        self.labels = one_hot(np.hstack([d["labels"] for d in data]), 10)
+        return self
+    
+    def next_batch(self, batch_size):
+        x, y = self.images[self._i:self._i+batch_size], self.labels[self._i:self._i+batch_size]
+        self._i = (self._i + batch_size) % len(self.images)
+        return x, y
+
+DATA_PATH = "CIFAR10/cifar10batchespy"
+
+def unpickle(file):
+    with open(os.path.join(DATA_PATH, file), 'rb') as fo:
+        dict = cPickle.load(fo, encoding='latin1')
+        return dict
+def one_hot(vec, vals=10):
+    n = len(vec)
+    out = np.zeros((n, vals))
+    out[range(n), vec] = 1
+    return out
+
+class CifarDataManager(object):
+    def __init__(self):
+        self.train = CifarLoader(["data_batch_{}".format(i) for i in range(1, 6)]).load()
+        self.test = CifarLoader(["test_batch"]).load()
+
+d = CifarDataManager()
+
+images = d.train.images
+labels = d.train.labels
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.optimizers import SGD
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+
+model = Sequential()
+model.add(Conv2D(16, (5, 5), input_shape=(32, 32, 3), padding='same', activation='relu'))
+model.add(Dropout(0.2))
+model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(1024, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(10, activation='softmax'))
+
+epochs = 10
+lrate = 0.1
+sgd = SGD(lr=lrate)
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(images, labels, epochs=epochs, batch_size=128)
+
+scores = model.evaluate(d.test.images, d.test.labels)
+print(scores)
